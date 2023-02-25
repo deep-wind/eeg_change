a509f3dfb7d5e749aa950e1184992f121b16321c
deep-wind <78983104+deep-wind@users.noreply.github.com>
2023-02-25 07:26:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -16,6 +16,8 @@ if uploaded_file is not None:
    try:
       df = pd.read_csv(uploaded_file)
       st.write(df)
+   except:
+      st.write("file not found")
       
 df = pd.read_csv("priyadharshini_1.txt",skiprows=6,header=None)
 df.columns=['index','channel1','channel2','channel3','channel4','channel5','channel6','channel7','channel8','acc1','acc2','acc3','time_std','timestamp']
